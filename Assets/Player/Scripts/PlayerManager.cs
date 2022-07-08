3b0f8a991133cf68294ec7dd02f65a75a33de250
Milas Baumgarten <milas.baumgarten@freenet.de>
2022-07-08 13:10:48 +0200

    minor fixes

--- Assets/Player/Scripts/PlayerManager.cs
+++ Assets/Player/Scripts/PlayerManager.cs
@@ -63,11 +63,6 @@ public class PlayerManager : MonoBehaviourPunCallbacks {
 	}
 
 	void OnSceneLoaded(Scene scene, LoadSceneMode loadingMode) {
-		// check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
-		if (!Physics.Raycast(transform.position, -Vector3.up, 5f)) {
-			transform.position = new Vector3(0f, 5f, 0f);
-		}
-
 		if (!localPlayerInstance && !PhotonNetwork.OfflineMode) {
 			SetupPlayer();
 		}
